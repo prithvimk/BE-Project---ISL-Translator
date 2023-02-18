18c09ee240152b6939e9f80a8e801ed2611d0f25
Prithvi Krishna <krishna.prithvi2001@gmail.com>
2023-02-18 12:26:42 +0530

    web_rtc based video call app

---
+++ web_rtc_videochat_app/client/src/SocketContext.js
@@ -0,0 +1,36 @@
+import React, { createContext, useState, useRef, useEffect } from "react";
+import { io } from "socket.io-client";
+import Peer from "simple-peer";
+
+const SocketContext = createContext();
+
+const socket = io("http://localhost:5000");
+
+const ContextProvider = ({ children }) => {
+
+    const [stream, setStream] = useState(null);
+
+    const myVideo = useRef();
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true})
+        .then((currentStream) => {
+            setStream(currentStream);
+
+            myVideo.current.srcObject = currentStream;
+        });
+
+        socket.on("me", (id) => setMe(id));
+    });
+    const answerCall = () => {
+
+    }
+
+    const callUser = () => {
+
+    }
+
+    const leaveCall = () => {
+
+    }
+}
